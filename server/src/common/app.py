007b5a0c9f3bd8d996aced09261431b19bcfee77
Derek Wu <ddwreck35@gmail.com>
2025-01-02 04:59:45 -0500

    add blueprint routes to app.

--- server/src/common/app.py
+++ server/src/common/app.py
@@ -7,3 +7,14 @@ app = Flask(__name__)
 app.config.from_object('config.Config')
 
 db.init_app(app)
+
+# Import and register blueprints
+from routes.employees.routes import employees_bp
+from routes.menu.routes import menu_bp
+from routes.seats.routes import seats_bp
+# from routes.parties.routes import parties_bp
+
+app.register_blueprint(employees_bp, url_prefix='/employees')
+app.register_blueprint(menu_bp, url_prefix='/menu')
+app.register_blueprint(seats_bp, url_prefix='/seats')
+# app.register_blueprint(parties_bp, url_prefix='/parties')
