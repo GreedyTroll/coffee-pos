043b662778193be04427b7dc06e46427eb9e3986
Derek Wu <ddwreck35@gmail.com>
2025-01-07 12:31:08 -0500

    update seats init data for dev convenience.

--- db/init_data.sql
+++ db/init_data.sql
@@ -1,16 +1,16 @@
 -- Inserting data into the Seats table
 INSERT INTO Seats (SeatID, Floor, PosX, PosY, PartyID)
 VALUES
-    ('11', 1, 1, 3, NULL),
-    ('12', 1, 1, 5, NULL),
-    ('21', 1, 2, 4, NULL),
-    ('22', 1, 2, 6, NULL),
-    ('31', 1, 3, 4, NULL),
-    ('32', 1, 3, 6, NULL),
-    ('41', 1, 7, 5, NULL),
-    ('42', 1, 8, 5, NULL),
-    ('51', 1, 10, 3, NULL),
-    ('52', 1, 10, 5, NULL),
-    ('61', 1, 7, 1, NULL),
-    ('BAR', 1, 4, 3, NULL),
-    ('BOSS', 1, 6, 3, NULL);
+    ('11', 1, 0, 3, NULL),
+    ('12', 1, 0, 5, NULL),
+    ('21', 1, 1, 4, NULL),
+    ('22', 1, 1, 6, NULL),
+    ('31', 1, 2, 4, NULL),
+    ('32', 1, 2, 6, NULL),
+    ('41', 1, 6, 5, NULL),
+    ('42', 1, 7, 5, NULL),
+    ('51', 1, 9, 3, NULL),
+    ('52', 1, 9, 5, NULL),
+    ('61', 1, 6, 1, NULL),
+    ('BAR', 1, 3, 3, NULL),
+    ('BOSS', 1, 5, 3, NULL);
