b6a6f72bf0dfa4db9248feafb03f14fc15207922
GreedyTroll <ddwreck35@gmail.com>
2025-05-17 16:47:05 +0800

    refactor the menu backend routes.

---
+++ server/src/routes/addons/routes.py
@@ -0,0 +1,211 @@
+from flask import Blueprint, request, jsonify
+from common.models import db, Item, Addon, LinkAddon, AddonGroup, LinkAddonGroup
+from sqlalchemy.exc import SQLAlchemyError
+import logging
+from common.models import model_to_dict
+from common.wrap import token_required
+
+logger = logging.getLogger()
+
+addons_bp = Blueprint('addons', __name__)
+
+@addons_bp.route('', methods=['GET'])
+def addons():
+    addons = db.session.query(Addon).outerjoin(AddonGroup, Addon.addongroup == AddonGroup.groupid).all()
+    result = []
+    for addon in addons:
+        addon_dict = model_to_dict(addon)
+        addon_group = AddonGroup.query.get(addon.addongroup) if addon.addongroup else None
+        addon_dict['groupname'] = addon_group.groupname if addon_group else None
+        addon_dict['groupid'] = addon_group.groupid if addon_group else None
+        result.append(addon_dict)
+    return jsonify(result)
+
+@addons_bp.route('/addonGroups', methods=['GET'])
+def addonGroups():
+    addon_groups = AddonGroup.query.all()
+    return jsonify([model_to_dict(addon_group) for addon_group in addon_groups])
+
+@addons_bp.route('/addAddonGroup', methods=['POST'])
+@token_required
+def addAddonGroup():
+    if request.is_json:
+        data = request.get_json()
+    else:
+        data = request.form.to_dict()
+    try:
+        addon_group = AddonGroup(
+            groupname=data['group_name']
+        )
+        db.session.add(addon_group)
+        db.session.commit()
+    except SQLAlchemyError as e:
+        db.session.rollback()
+        logger.error(f'{e}')
+        return {'Error': f'{e}'}, 500
+    return {'groupid': addon_group.groupid, 'success': True}, 200
+
+@addons_bp.route('/addonGroup/<int:id>', methods=['DELETE'])
+@token_required
+def deleteAddonGroup(id):
+    if not id:
+        return {'error': 'no id provided'}, 400
+    try:
+        addon_group = AddonGroup.query.get(id)
+        if not addon_group:
+            return {'error': "addon group id not found"}, 400
+        db.session.delete(addon_group)
+        db.session.commit()
+    except SQLAlchemyError as e:
+        db.session.rollback()
+        logger.error(f'{e}')
+        return {'Error': f'{e}'}, 500
+    return {'success': True}, 200
+
+@addons_bp.route('/addAddon', methods=['POST'])
+@token_required
+def addAddon():
+    if request.is_json:
+        data = request.get_json()
+    else:
+        data = request.form.to_dict()
+    
+    # Check if data is received
+    if data is None:
+        return {"message": 'no data received'}, 400
+    try:
+        if 'addon_group' in data:
+            addon_group = AddonGroup.query.get(int(data.get('addon_group')))
+            if not addon_group:
+                return {'error': 'addon group not found'}, 400
+        else:
+            addon_group = None
+        addon = Addon(
+            addongroup=addon_group.groupid if addon_group else None,
+            addonname=data['addon_name'],
+            price=float(data['price'])
+        )
+        db.session.add(addon)
+        db.session.commit()
+    except SQLAlchemyError as e:
+        db.session.rollback()
+        logger.error(f'{e}')
+        return {'Error': f'{e}'}, 500
+    
+    return {'addonid': addon.addonid, 'success': True}, 200
+
+@addons_bp.route('/linkAddon', methods=['POST'])
+@token_required
+def linkAddon():
+    if request.is_json:
+        data = request.get_json()
+    else:
+        data = request.form.to_dict()
+    
+    if data is None:
+        return {"message": 'no data received'}, 400
+    
+    try:
+        product_id = data['product_id']
+        if not product_id:
+            return {'error': 'no product id provided'}, 400
+        product = Item.query.get(product_id)
+        if not product:
+            return {'error': 'product not found'}, 400
+        if group_ids := data.get('group_ids'):
+            group_ids = [int(group_id) for group_id in group_ids.split(',')]
+            for group_id in group_ids:
+                addon_group = AddonGroup.query.get(group_id)
+                if not addon_group:
+                    return {'error': f'addon group with id {group_id} not found'}, 400 
+
+                existing_link = LinkAddonGroup.query.filter_by(itemid=product_id, groupid=addon_group.groupid).first()
+                if existing_link:
+                    db.session.delete(existing_link)
+                else:
+                    db.session.add(LinkAddonGroup(itemid=product_id, groupid=addon_group.groupid))
+        elif addon_ids := data.get('addon_ids'):
+            addon_ids = [int(addon_id) for addon_id in addon_ids.split(',')]
+            addons = Addon.query.filter(Addon.addonid.in_(addon_ids)).all()
+            for addon in addons:
+                if addon.addongroup is not None:
+                    return {'error': 'addon can only be linked by addon groups'}, 400
+            existing_addons = {available_addon.addonid for available_addon in LinkAddon.query.filter_by(itemid=product_id).all()}
+            new_addons = set(addon_ids)
+            addons_to_remove = existing_addons - new_addons
+            addons_to_add = new_addons - existing_addons
+
+            for addon_id in addons_to_remove:
+                LinkAddon.query.filter_by(itemid=product_id, addonid=addon_id).delete()
+            for addon_id in addons_to_add:
+                db.session.add(LinkAddon(itemid=product_id, addonid=addon_id))
+        else:
+            return {'error': 'no addon ids provided'}, 400
+        
+        db.session.commit()
+    except SQLAlchemyError as e:
+        db.session.rollback()
+        logger.error(f'{e}')
+        return {'Error': f'{e}'}, 500
+    
+    return {'success': True}, 200
+
+@addons_bp.route('/updateAddonGroup', methods=['PUT'])
+@token_required
+def updateAddonGroup():
+    if request.is_json:
+        data = request.get_json()
+    else:
+        data = request.form.to_dict()
+    
+    # Check if data is received
+    if data is None:
+        return {"message": 'no data received'}, 400
+
+    addon_id = data.get('addon_id')
+    if not addon_id:
+        return {'error': 'no addon id provided'}, 400
+    addon = Addon.query.get(addon_id)
+    if not addon:
+        return {'error': "addon id not found"}, 400
+
+    group_id = data.get('group_id')
+    if not group_id:
+        group_id = None
+
+    if group_id == None:
+        addon_group = None 
+    else:
+        addon_group = AddonGroup.query.get(group_id)
+        if not addon_group:
+            return {'error': "addon group id not found"}, 400
+    
+    try:
+        addon.addongroup = group_id
+        db.session.commit()
+    except SQLAlchemyError as e:
+        db.session.rollback()
+        logger.error(f'{e}')
+        return {'Error': f'{e}'}, 500
+    
+    return {'success': True}, 200
+
+@addons_bp.route('/<int:id>', methods=['DELETE'])
+@token_required
+def deleteAddon(id):
+    if not id:
+        return {'error': 'no id provided'}, 400
+    
+    addon = Addon.query.get(id)
+    if not addon:
+        return {'error': "addon id not found"}, 400
+    
+    try:
+        db.session.delete(addon)
+        db.session.commit()
+    except SQLAlchemyError as e:
+        db.session.rollback()
+        logger.error(f'{e}')
+        return {'Error': f'{e}'}, 500
+
+    return {'success': True}, 200
--- server/src/routes/menu/routes.py
+++ server/src/routes/menu/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from common.models import db, Item, Category, Tag, ItemTag, Addon, AvailableAddon, AddonGroup
+from common.models import db, Item, Category, Tag, ItemTag, Addon, LinkAddon, LinkAddonGroup, AddonGroup
 from sqlalchemy.exc import SQLAlchemyError
 import logging
 from common.models import model_to_dict
@@ -16,13 +16,16 @@ def menu():
     
     item_tags = db.session.query(ItemTag).all()
     tags = db.session.query(Tag).all()
-    available_addons = db.session.query(AvailableAddon).all()
+    available_addons = db.session.query(LinkAddon).all()
     addons = db.session.query(Addon, AddonGroup.groupname).outerjoin(AddonGroup, Addon.addongroup == AddonGroup.groupid).all()
+    link_groups = db.session.query(LinkAddonGroup).all()
+
     addon_dict = {}
     for addon, groupname in addons:
         addon_data = model_to_dict(addon)
         addon_data['groupname'] = groupname
         addon_dict[addon.addonid] = addon_data
+
     tag_dict = {tag.tagid: model_to_dict(tag) for tag in tags}
 
     item_tag_map = {}
@@ -36,7 +39,12 @@ def menu():
         if available_addon.itemid not in item_addon_map:
             item_addon_map[available_addon.itemid] = []
         item_addon_map[available_addon.itemid].append(addon_dict[available_addon.addonid])
-
+    
+    item_addongroup_map = {}
+    for link_group in link_groups:
+        if link_group.itemid not in item_addongroup_map:
+            item_addongroup_map[link_group.itemid] = []
+        item_addongroup_map[link_group.itemid].append(link_group.groupid)
     menu = []
     for category in categories:
         category_dict = model_to_dict(category)
@@ -45,6 +53,7 @@ def menu():
             item_id = item_dict['productid']
             item_dict['tags'] = item_tag_map.get(item_id, [])
             item_dict['addons'] = item_addon_map.get(item_id, [])
+            item_dict['addongroups'] = item_addongroup_map.get(item_id, [])
         category_dict['items'] = category_items
         menu.append(category_dict)
 
@@ -87,7 +96,7 @@ def addItem():
             menuorder=data.get('menu_order', 0),
             description=data.get('description', ''),
             price=float(data.get('price', 0)),
-            remainingstock=int(data.get('remaining_stock', None)),
+            remainingstock=data.get('remaining_stock', None),
             categoryid=category.categoryid
         )
     
@@ -98,7 +107,7 @@ def addItem():
         logger.error(f'{e}')
         return {'Error': f'{e}'}, 500
     
-    return {'category': category.categoryid, 'item': new_item.productid, 'success': True}, 200
+    return {'category': category.categoryid, 'productid': new_item.productid, 'success': True}, 200
 
 @menu_bp.route('/item/<int:id>', methods=['PUT'])
 @token_required
@@ -248,300 +257,6 @@ def deleteCategory(id):
     
     try:
         db.session.delete(category)
-        db.session.commit()
-    except SQLAlchemyError as e:
-        db.session.rollback()
-        logger.error(f'{e}')
-        return {'Error': f'{e}'}, 500
-
-    return {'success': True}, 200
-
-#############
-#    TAGS   #
-#############
-@menu_bp.route('/tags', methods=['GET'])
-def tags():
-    tags = Tag.query.all()
-    return jsonify([model_to_dict(tag) for tag in tags])
-
-@menu_bp.route('/addTag', methods=['POST'])
-@token_required
-def addTag():
-    if request.is_json:
-        data = request.get_json()
-    else:
-        data = request.form.to_dict()
-    
-    # Check if data is received
-    if data is None:
-        return {"message": 'no data received'}, 400
-    
-    try:
-        tag = Tag(
-            tagname=data['tag_name'],
-            tagcolor=data.get('tag_color', '#000000')
-        )
-        db.session.add(tag)
-        db.session.commit()
-    except SQLAlchemyError as e:
-        db.session.rollback()
-        logger.error(f'{e}')
-        return {'Error': f'{e}'}, 500
-    
-    return {'tagid': tag.tagid, 'success': True}, 200
-
-@menu_bp.route('/linkTag', methods=['POST'])
-@token_required
-def linkTag():
-    if request.is_json:
-        data = request.get_json()
-    else:
-        data = request.form.to_dict()
-    
-    # Check if data is received
-    if data is None:
-        return {"message": 'no data received'}, 400
-    
-    try:
-        tag_ids = data.get('tag_ids', [])
-        if 'category_id' in data:
-            items = Item.query.filter_by(categoryid=data['category_id']).all()
-            for item in items:
-                existing_tags = {item_tag.tagid for item_tag in ItemTag.query.filter_by(itemid=item.productid).all()}
-                new_tags = set(tag_ids)
-                tags_to_remove = existing_tags - new_tags
-                tags_to_add = new_tags - existing_tags
-
-                for tag_id in tags_to_remove:
-                    ItemTag.query.filter_by(itemid=item.productid, tagid=tag_id).delete()
-                for tag_id in tags_to_add:
-                    db.session.add(ItemTag(itemid=item.productid, tagid=tag_id))
-        else:
-            product_id = data['product_id']
-            existing_tags = {item_tag.tagid for item_tag in ItemTag.query.filter_by(itemid=product_id).all()}
-            new_tags = set(tag_ids)
-            tags_to_remove = existing_tags - new_tags
-            tags_to_add = new_tags - existing_tags
-
-            for tag_id in tags_to_remove:
-                ItemTag.query.filter_by(itemid=product_id, tagid=tag_id).delete()
-            for tag_id in tags_to_add:
-                db.session.add(ItemTag(itemid=product_id, tagid=tag_id))
-        
-        db.session.commit()
-    except SQLAlchemyError as e:
-        db.session.rollback()
-        logger.error(f'{e}')
-        return {'Error': f'{e}'}, 500
-    
-    return {'success': True}, 200
-
-@menu_bp.route('/tags/<int:id>', methods=['DELETE'])
-@token_required
-def deleteTag(id):
-    if not id:
-        return {'error': 'no id provided'}, 400
-    
-    tag = Tag.query.get(id)
-    if not tag:
-        return {'error': "tag id not found"}, 400
-    
-    try:
-        db.session.delete(tag)
-        db.session.commit()
-    except SQLAlchemyError as e:
-        db.session.rollback()
-        logger.error(f'{e}')
-        return {'Error': f'{e}'}, 500
-
-    return {'success': True}, 200
-
-###########
-# ADDONS  #
-###########
-@menu_bp.route('/addons', methods=['GET'])
-def addons():
-    addons = db.session.query(Addon).outerjoin(AddonGroup, Addon.addongroup == AddonGroup.groupid).all()
-    result = []
-    for addon in addons:
-        addon_dict = model_to_dict(addon)
-        addon_group = AddonGroup.query.get(addon.addongroup) if addon.addongroup else None
-        addon_dict['groupname'] = addon_group.groupname if addon_group else None
-        addon_dict['groupid'] = addon_group.groupid if addon_group else None
-        result.append(addon_dict)
-    return jsonify(result)
-
-@menu_bp.route('/addonGroups', methods=['GET'])
-def addonGroups():
-    addon_groups = AddonGroup.query.all()
-    return jsonify([model_to_dict(addon_group) for addon_group in addon_groups])
-
-@menu_bp.route('/addAddonGroup', methods=['POST'])
-@token_required
-def addAddonGroup():
-    if request.is_json:
-        data = request.get_json()
-    else:
-        data = request.form.to_dict()
-    try:
-        addon_group = AddonGroup(
-            groupname=data['group_name']
-        )
-        db.session.add(addon_group)
-        db.session.commit()
-    except SQLAlchemyError as e:
-        db.session.rollback()
-        logger.error(f'{e}')
-        return {'Error': f'{e}'}, 500
-    return {'groupid': addon_group.groupid, 'success': True}, 200
-
-@menu_bp.route('/addonGroup/<int:id>', methods=['DELETE'])
-@token_required
-def deleteAddonGroup(id):
-    if not id:
-        return {'error': 'no id provided'}, 400
-    try:
-        addon_group = AddonGroup.query.get(id)
-        if not addon_group:
-            return {'error': "addon group id not found"}, 400
-        db.session.delete(addon_group)
-        db.session.commit()
-    except SQLAlchemyError as e:
-        db.session.rollback()
-        logger.error(f'{e}')
-        return {'Error': f'{e}'}, 500
-    return {'success': True}, 200
-
-@menu_bp.route('/addAddon', methods=['POST'])
-@token_required
-def addAddon():
-    if request.is_json:
-        data = request.get_json()
-    else:
-        data = request.form.to_dict()
-    
-    # Check if data is received
-    if data is None:
-        return {"message": 'no data received'}, 400
-    try:
-        if 'addon_group' in data:
-            addon_group = AddonGroup.query.get(int(data.get('addon_group')))
-            if not addon_group:
-                return {'error': 'addon group not found'}, 400
-        else:
-            addon_group = None
-        addon = Addon(
-            addongroup=addon_group.groupid if addon_group else None,
-            addonname=data['addon_name'],
-            price=float(data['price'])
-        )
-        db.session.add(addon)
-        db.session.commit()
-    except SQLAlchemyError as e:
-        db.session.rollback()
-        logger.error(f'{e}')
-        return {'Error': f'{e}'}, 500
-    
-    return {'addonid': addon.addonid, 'success': True}, 200
-
-@menu_bp.route('/linkAddon', methods=['POST'])
-@token_required
-def linkAddon():
-    if request.is_json:
-        data = request.get_json()
-    else:
-        data = request.form.to_dict()
-    
-    # Check if data is received
-    if data is None:
-        return {"message": 'no data received'}, 400
-    
-    try:
-        addon_ids = data.get('addon_ids', [])
-        if 'category_id' in data:
-            items = Item.query.filter_by(categoryid=data['category_id']).all()
-            for item in items:
-                existing_addons = {available_addon.addonid for available_addon in AvailableAddon.query.filter_by(itemid=item.productid).all()}
-                new_addons = set(addon_ids)
-                addons_to_remove = existing_addons - new_addons
-                addons_to_add = new_addons - existing_addons
-
-                for addon_id in addons_to_remove:
-                    AvailableAddon.query.filter_by(itemid=item.productid, addonid=addon_id).delete()
-                for addon_id in addons_to_add:
-                    db.session.add(AvailableAddon(itemid=item.productid, addonid=addon_id))
-        else:
-            product_id = data['product_id']
-            existing_addons = {available_addon.addonid for available_addon in AvailableAddon.query.filter_by(itemid=product_id).all()}
-            new_addons = set(addon_ids)
-            addons_to_remove = existing_addons - new_addons
-            addons_to_add = new_addons - existing_addons
-
-            for addon_id in addons_to_remove:
-                AvailableAddon.query.filter_by(itemid=product_id, addonid=addon_id).delete()
-            for addon_id in addons_to_add:
-                db.session.add(AvailableAddon(itemid=product_id, addonid=addon_id))
-        
-        db.session.commit()
-    except SQLAlchemyError as e:
-        db.session.rollback()
-        logger.error(f'{e}')
-        return {'Error': f'{e}'}, 500
-    
-    return {'success': True}, 200
-
-@menu_bp.route('/updateAddonGroup', methods=['PUT'])
-@token_required
-def updateAddonGroup():
-    if request.is_json:
-        data = request.get_json()
-    else:
-        data = request.form.to_dict()
-    
-    # Check if data is received
-    if data is None:
-        return {"message": 'no data received'}, 400
-
-    addon_id = data.get('addon_id')
-    if not addon_id:
-        return {'error': 'no addon id provided'}, 400
-    addon = Addon.query.get(addon_id)
-    if not addon:
-        return {'error': "addon id not found"}, 400
-
-    group_id = data.get('group_id')
-    if not group_id:
-        group_id = None
-
-    if group_id == None:
-        addon_group = None 
-    else:
-        addon_group = AddonGroup.query.get(group_id)
-        if not addon_group:
-            return {'error': "addon group id not found"}, 400
-    
-    try:
-        addon.addongroup = group_id
-        db.session.commit()
-    except SQLAlchemyError as e:
-        db.session.rollback()
-        logger.error(f'{e}')
-        return {'Error': f'{e}'}, 500
-    
-    return {'success': True}, 200
-
-@menu_bp.route('/addons/<int:id>', methods=['DELETE'])
-@token_required
-def deleteAddon(id):
-    if not id:
-        return {'error': 'no id provided'}, 400
-    
-    addon = Addon.query.get(id)
-    if not addon:
-        return {'error': "addon id not found"}, 400
-    
-    try:
-        db.session.delete(addon)
         db.session.commit()
     except SQLAlchemyError as e:
         db.session.rollback()
