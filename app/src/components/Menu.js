106a334800865bf30fad7fafdace0078d2bffb42
GreedyTroll <ddwreck35@gmail.com>
2025-05-17 16:45:53 +0800

    refactor the menu components.

--- app/src/components/Menu.js
+++ app/src/components/Menu.js
@@ -1,7 +1,6 @@
 import useAxios from '../hooks/useAxiosAuth';
 import React, { useState, useEffect } from 'react';
 import MenuSection from './MenuSection';
-import TagAddonLinker from './TagAddonLinker';
 import TagAddonManager from './TagAddonManager';
 import './Menu.css';
 
@@ -9,11 +8,6 @@ const apiUrl = process.env.REACT_APP_API_URL;
 
 const Menu = ({ isAuthenticated }) => {
     const [menu, setMenu] = useState([]);
-    const [tags, setTags] = useState([]);
-    const [addons, setAddons] = useState([]);
-    const [newTags, setNewTags] = useState({});
-    const [newAddons, setNewAddons] = useState({});
-    const [popupItem, setPopupItem] = useState(null);
     const [popup, setPopup] = useState(false);
 
     const axios = useAxios();
@@ -31,52 +25,7 @@ const Menu = ({ isAuthenticated }) => {
         .catch(error => {
             console.error('Error fetching data:', error);
         });
-        // get tags
-        axios.get(`${apiUrl}/menu/tags`)
-        .then(response => {
-            setTags(response.data);
-        })
-        .catch(error => {
-            console.error('Error fetching tags:', error);
-        });
-        // get addons
-        axios.get(`${apiUrl}/menu/addons`)
-        .then(response => {
-            setAddons(response.data);
-        })
-        .catch(error => {
-            console.error('Error fetching addons:', error);
-        });
-
     }, []);
-
-    const handleTagChange = (categoryid, productid, e) => {
-        const { name, value } = e.target;
-        setNewTags(prev => ({
-            ...prev,
-            [categoryid]: {
-                ...(prev[categoryid] || {}),
-                [productid]: {
-                    ...((prev[categoryid] || {})[productid] || {}),
-                    [name]: value
-                }
-            }
-        }));
-    };
-
-    const handleAddonChange = (categoryid, productid, e) => {
-        const { name, value } = e.target;
-        setNewAddons(prev => ({
-            ...prev,
-            [categoryid]: {
-                ...(prev[categoryid] || {}),
-                [productid]: {
-                    ...((prev[categoryid] || {})[productid] || {}),
-                    [name]: value
-                }
-            }
-        }));
-    };
 
     const updateCategory = (category, categoryId) => {
         // if category is null, delete the category
@@ -99,43 +48,6 @@ const Menu = ({ isAuthenticated }) => {
         } else { // add new category
             setMenu(prev => [...prev, { ...category, categoryid: categoryId, items: [] }]);
         }
-        // update the tags/addons for the category
-        if (category && category.items) {
-            const catId = category.categoryid || categoryId;
-            category.items.forEach((item) => {
-                const productId = item.productid;
-                if (newTags[catId] && newTags[catId][productId]?.tags) {
-                    const tag_ids = newTags[catId][productId].tags.map(tag => tag.tagid);
-                    axios.post(`${apiUrl}/menu/linkTag`, { product_id: productId, tag_ids })
-                    .catch(error => console.error('Error updating tags:', error));
-                }
-                if (newAddons[catId] && newAddons[catId][productId]?.addons) {
-                    const addon_ids = newAddons[catId][productId].addons.map(addon => addon.addonid);
-                    axios.post(`${apiUrl}/menu/linkAddon`, { product_id: productId, addon_ids })
-                    .catch(error => console.error('Error updating addons:', error));
-                }
-            });
-        }
-    };
-
-    const cancelChanges = (categoryid) => {
-        // Clear new tags and addons
-        setNewTags(prev => {
-            delete prev[categoryid];
-            return prev;
-        });
-        setNewAddons(prev => {
-            delete prev[categoryid];
-            return prev;
-        });
-    }
-
-    const openPopup = (item) => {
-        setPopupItem(item);
-    };
-
-    const closePopupItem = () => {
-        setPopupItem(null);
     };
 
     const closePopup = () => {
@@ -158,28 +70,15 @@ const Menu = ({ isAuthenticated }) => {
                 <MenuSection
                     key={index}
                     category={category}
-                    openPopup={openPopup}
                     isAuthenticated={isAuthenticated}
                     updateCategory={updateCategory}
-                    cancelChanges={cancelChanges}
                 />
             ))}
-            <MenuSection
+            {<MenuSection
                 category={{ categoryid: -1, categoryname: '', description: '', menuorder: 0, items: [] }}
-                openPopup={openPopup}
                 isAuthenticated={isAuthenticated}
                 updateCategory={updateCategory}
-            />
-            {popupItem && (
-                <TagAddonLinker
-                    allTags={tags}
-                    allAddons={addons}
-                    item={popupItem}
-                    closePopup={closePopupItem}
-                    handleTagChange={handleTagChange}
-                    handleAddonChange={handleAddonChange}
-                />
-            )}
+            />}
             {popup && (
                 <TagAddonManager 
                     closePopup={closePopup}
---
+++ app/src/components/MenuItem.js
@@ -0,0 +1,144 @@
+import React, { useEffect, useState } from 'react';
+import './MenuItem.css';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+
+const MenuItem = ({ itemDeets, updateItem, addontagLink, editMode, addDeleteItem }) => {
+    const [item, setItem] = useState(itemDeets);
+    const [isEditMode, setIsEditMode] = useState(false);
+
+    useEffect(() => {
+        setItem(itemDeets);
+        if(!itemDeets){
+            console.log("itemDeets is null");
+        }
+    }, [itemDeets]);
+
+    useEffect(() => {
+        setIsEditMode(editMode);
+    }, [editMode]);
+
+    const deleteItem = (item) => {
+        addDeleteItem(item.productid);
+    };
+
+    const toggleEditStock = () => {
+        if(item.productid < 0) {
+            setItem({ ...item, remainingstock: item.remainingstock === null ? 0 : null });
+            return;
+        }
+        updateItem({ ...item, remainingstock: item.remainingstock === null ? 0 : null });
+        // If remainingstock is null, set it to 0, otherwise set it to null
+    };
+
+    const toggleVisibility = () => {
+        if(item.productid < 0) {
+            setItem({ ...item, ishidden: !item.ishidden });
+            return;
+        }
+        updateItem({ ...item, ishidden: !item.ishidden });
+    };
+
+    const handleProductChange = (e) => {
+        if(item.productid < 0) {
+            setItem({...item, [e.target.name]: e.target.value});
+            return;
+        }
+        else {
+            const { name, value } = e.target;
+            updateItem({...item, [name]: value});
+        }
+    }
+    
+    const addNewProduct = () => {
+        if(item.productname === "" || item.price === ""){
+            alert("Please fill in all fields");
+            return;
+        }
+        updateItem(item);
+    }
+
+    if(!isEditMode && item.ishidden) { 
+        return (<></>);
+    }
+    return (
+        <div className="menu-row">
+            {item.productid < 0 && (
+                <>
+                <input
+                    type="text"
+                    name="productname"
+                    placeholder="Product Name"
+                    value={item.productname}
+                    onChange={handleProductChange}
+                    className="menu-item-name-edit"
+                />
+                <input
+                    type="text"
+                    name="description"
+                    placeholder="Description"
+                    value={item.description}
+                    onChange={handleProductChange}
+                    className="menu-item-description-edit"
+                />
+                </>
+            )}
+            {item.productid >= 0 && (
+                <>
+                <div className="menu-item-name">
+                    {item.productname || "No Product Name"}
+                        {item.remainingstock !== null && ` (${item.remainingstock})`}
+                </div>
+                <div className="menu-item-description">{item.description || ""}</div>
+                </>
+            )}
+            {!isEditMode && (
+                <div className="menu-item-price">{item.price ? `${Math.round(item.price)}` : 0}</div>
+            )}
+            {isEditMode && (
+            <>
+                <input 
+                    type="number"
+                    name="price"
+                    value={Math.round(item.price)}
+                    onChange={(e) => handleProductChange(e)}
+                    className="menu-item-price-edit"
+                />
+                <input
+                    type="number"
+                    name="menuorder"
+                    value={item.menuorder}
+                    onChange={(e) => handleProductChange(e)}
+                    className="menu-item-order"
+                />
+                {item.remainingstock !== null ? (
+                    <input
+                        type="number"
+                        name="remainingstock"
+                        value={item.remainingstock}
+                        onChange={(e) => handleProductChange(e)}
+                        className="menu-item-stock"
+                    />
+                    ) : (
+                    <span></span>
+                    )}
+                    <button onClick={() => toggleEditStock()}>
+                        {item.remainingstock !== null ? "No Stock" : "Edit Stock"}
+                    </button>
+                    <button onClick={() => toggleVisibility()}>
+                        {item.ishidden ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                    </button>
+                    <button className="delete-button" onClick={() => deleteItem(item)}>Delete</button>
+                    {item.productid < 0 ? (
+                    <button className="add-button" onClick={() => addNewProduct()}>Add Product</button>
+                    ) : (
+                    <button className="edit-button" onClick={() => addontagLink(item)}>Tags Addons</button>
+                    )
+                }
+            </>
+            )}
+        </div>
+    );
+};
+
+export default MenuItem;
