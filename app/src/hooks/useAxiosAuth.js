28801b59cca2a59085a79e869a85214b20b4dc45
GreedyTroll <ddwreck35@gmail.com>
2025-05-22 12:35:54 +0800

    reroute to login page if unauthorized with axios.

--- app/src/hooks/useAxiosAuth.js
+++ app/src/hooks/useAxiosAuth.js
@@ -16,6 +16,16 @@ const useAxios = () => {
     error => Promise.reject(error)
   );
 
+  axiosInstance.interceptors.response.use(
+    response => response,
+    error => {
+      if (error.response && error.response.status === 401) {
+        window.location.href = '/login';
+      }
+      return Promise.reject(error);
+    }
+  );
+
   return axiosInstance;
 };
 
